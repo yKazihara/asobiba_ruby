485394222b859bf68fcf2ed0f13e2dd6213bcce1
yKazihara <run5150@gmail.com>
2024-12-23 21:40:50 +0900

    Hello worldとWhileサンプル。

--- sample.rb
+++ sample.rb
@@ -1,2 +1,1 @@
-# frozen_string_literal: true
-p(1 + 1) # 2
+puts "Hello World"
---
+++ while_sample.rb
@@ -0,0 +1,21 @@
+num = 0
+while num < 10 do
+  if num == 0
+    puts num.to_s + " is Zero"
+  elsif num > 5 then
+    puts num.to_s + " is over 5"
+  else
+    puts num.to_s + " is under 5"
+  end
+  num = num + 1
+end
+#0 is Zero
+# 1 is under 5
+# 2 is under 5
+# 3 is under 5
+# 4 is under 5
+# 5 is under 5
+# 6 is over 5
+# 7 is over 5
+# 8 is over 5
+# 9 is over 5
