6699349714695eca0a5f2da52728e28830311421
yKazihara <run5150@gmail.com>
2025-01-01 22:20:21 +0900

    ifのサンプル。

---
+++ sample/if_sample.rb
@@ -0,0 +1,25 @@
+drink = "milk"
+
+if drink == "coffee" then
+  puts "COFFEEだよ"
+elsif drink == "milk" then
+  puts "MILKだよ"
+else
+  puts "それ以外だよ"
+end
+# MILKだよ
+
+puts "-----"
+
+age = 20
+puts "18歳以上です" if age >= 18
+# 18歳以上です
+
+puts "-----"
+
+var = "orange"
+
+unless var == "apple" then
+  puts "りんごじゃないみたい"
+end
+# りんごじゃないみたい
